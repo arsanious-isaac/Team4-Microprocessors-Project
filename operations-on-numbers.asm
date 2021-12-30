c61879d004036ad809a25d0106468e771b94a4a2
Clara Raef Nasry <clara180550@feng.bu.edu.eg>
2021-12-30 15:42:45 +0200

    Update operations-on-numbers.asm

--- operations-on-numbers.asm
+++ operations-on-numbers.asm
@@ -151,7 +151,14 @@ StandardOperation proc
 
 ;CLARA
 avgOperation proc
-
+pusha
+ pushf
+ 
+ mov sum,0
+ mov avg,0
+ 
+ mov cx ,6
+ mov si ,0
 
 
 
@@ -161,98 +168,69 @@ avgOperation proc
 Sort proc
  pusha
  pushf
- 
  mov cx,6
  mov si,0
  
  addTo:
- 
  mov dl, nums[si]
  mov asc[si],dl
- 
  inc si
  
  loop addTo
  
- 
  ; Selection sort
- 
- ; 1 2 3 5 4 6
- 
- 
  
   mov si,0
   mov cx,5
   
   p1:
   push cx
-  
   mov cx,5
   sub cx,si
-  
   mov di, si ;si =0
   inc di    ;di =1
-  
   mov bx, si ;bx= 0
   
   p2:
-  
   mov dl, asc[bx]
   cmp asc[di],dl
   jnb fp2
-  
   mov bx,di
  
   fp2:
   inc di
+  
   loop p2 
   
   mov dl,asc[bx]
   mov al,asc[si]
-  
   mov asc[si],dl
   mov asc[bx],al
-  
   pop cx 
   inc si
   
   loop p1  
-  
+
   mov di,0
   mov si,5
   mov cx,6
-  
-  
 
  insert:  
-  
-
-  
   mov dl,asc[si]
   mov des[di],dl
-  
   inc di
   dec si
-  
   loop insert
-  
 
 mov al,des[5]
 mov min,al
-
-  
 mov al,des[0]
 mov max,al  
-  
-  
-  
-  
  
  popf
  popa
     ret
     Sort endp
-
 
 toEnd:
     end main  
